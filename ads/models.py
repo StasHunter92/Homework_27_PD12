19ec6be9c0522a06910e7b347fdbe0eb956cd098
StasHunter92 <stashunter92@gmail.com>
2023-02-03 22:11:19 +0300

    Add typing

--- ads/models.py
+++ ads/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # ----------------------------------------------------------------------------------------------------------------------
 # Create category model
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name: str = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -13,14 +13,14 @@ class Category(models.Model):
 # ----------------------------------------------------------------------------------------------------------------------
 # Create ad model
 class Ad(models.Model):
-    PUBLISHED = [(True, "Опубликовано"), (False, "Не опубликовано")]
+    PUBLISHED: list[tuple] = [(True, "Опубликовано"), (False, "Не опубликовано")]
 
-    name = models.CharField(max_length=200)
-    author = models.CharField(max_length=50)
-    price = models.PositiveIntegerField()
-    description = models.CharField(max_length=1000)
-    address = models.CharField(max_length=200)
-    is_published = models.BooleanField(choices=PUBLISHED, default=False)
+    name: str = models.CharField(max_length=200)
+    author: str = models.CharField(max_length=50)
+    price: int = models.PositiveIntegerField()
+    description: str = models.CharField(max_length=1000)
+    address: str = models.CharField(max_length=200)
+    is_published: bool = models.BooleanField(choices=PUBLISHED, default=False)
 
     def __str__(self):
         return self.name
