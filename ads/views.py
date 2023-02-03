798cbfe52a7b1b08b02ed45208e4652bd71ea1a8
StasHunter92 <stashunter92@gmail.com>
2023-02-03 22:11:45 +0300

    Fix code and add typing

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,7 @@
 import json
+from json import JSONDecodeError
 
+from django.db.models import QuerySet
 from django.http import JsonResponse, Http404
 from django.utils.decorators import method_decorator
 from django.views import View
@@ -11,7 +13,13 @@ from ads.models import Ad, Category
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Start page (FBV)
-def index(request):
+def index(request) -> JsonResponse:
+    """
+    Root view that returns a JSON response indicating success
+
+    :param request: The incoming request object
+    :return: JSON "OK" status
+    """
     return JsonResponse({"status": "ok"}, status=200)
 
 
@@ -19,9 +27,15 @@ def index(request):
 # Ads page (CBV)
 @method_decorator(csrf_exempt, name="dispatch")
 class AdView(View):
-    def get(self, request):
-        ads = Ad.objects.all()
-        response = []
+    def get(self, request) -> JsonResponse:
+        """
+        Handle a GET request to the AdView. Returns a list of all Ad objects in the database as a JSON response
+
+        :param request: The incoming request object
+        :return: A JSON response with a list of dictionaries, where each dictionary represents an Ad object
+        """
+        ads: QuerySet[Ad] = Ad.objects.all()
+        response: list[dict] | list = []
 
         for ad in ads:
             response.append({
@@ -33,10 +47,19 @@ class AdView(View):
 
         return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii": False}, status=200)
 
-    def post(self, request):
-        ad_data = json.loads(request.body)
+    def post(self, request) -> JsonResponse:
+        """
+        Handle a POST request to the AdView. Creates a new Ad object in the database
 
-        ad = Ad.objects.create(
+        :param request: The incoming request object
+        :return: A JSON response with a dictionary representing the newly created Ad object
+        """
+        try:
+            ad_data: dict = json.loads(request.body)
+        except JSONDecodeError:
+            return JsonResponse({"status": "Wrong data"}, status=400)
+
+        ad: Ad = Ad.objects.create(
             name=ad_data.get("name"),
             author=ad_data.get("author"),
             price=ad_data.get("price"),
@@ -45,7 +68,7 @@ class AdView(View):
             is_published=ad_data.get("is_published")
         )
 
-        response = {
+        response: dict = {
             "id": ad.id,
             "name": ad.name,
             "author": ad.author,
@@ -62,15 +85,21 @@ class AdView(View):
 # Single ad page (CBV)
 @method_decorator(csrf_exempt, name="dispatch")
 class AdDetailView(DetailView):
-    model = Ad
+    model: Ad = Ad
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> JsonResponse:
+        """
+        Retrieve a single Ad instance
+
+        :param request: The incoming request object
+        :return: JSON response with Ad data
+        """
         try:
-            ad = self.get_object()
+            ad: Ad = self.get_object()
         except Http404:
             return JsonResponse({"error": "Not found"}, status=404)
 
-        response = {
+        response: dict = {
             "id": ad.id,
             "name": ad.name,
             "author": ad.author,
@@ -86,9 +115,16 @@ class AdDetailView(DetailView):
 # Categories page (CBV)
 @method_decorator(csrf_exempt, name="dispatch")
 class CategoryView(View):
-    def get(self, request):
-        categories = Category.objects.all()
-        response = []
+    def get(self, request) -> JsonResponse:
+        """
+        Handle a GET request to the CategoryView.
+        Returns a list of all Category objects in the database as a JSON response
+
+        :param request: The incoming request object
+        :return: A JSON response with a list of dictionaries, where each dictionary represents a Category object
+        """
+        categories: QuerySet[Category] = Category.objects.all()
+        response: list = []
 
         for category in categories:
             response.append({
@@ -98,14 +134,23 @@ class CategoryView(View):
 
         return JsonResponse(response, safe=False, json_dumps_params={"ensure_ascii": False}, status=200)
 
-    def post(self, request):
-        category_data = json.loads(request.body)
+    def post(self, request) -> JsonResponse:
+        """
+        Handle a POST request to the CategoryView. Creates a new Category object in the database
 
-        category = Category.objects.create(
+        :param request: The incoming request object
+        :return: A JSON response with a dictionary representing the newly created Category object
+        """
+        try:
+            category_data: dict = json.loads(request.body)
+        except JSONDecodeError:
+            return JsonResponse({"status": "Wrong data"}, status=400)
+
+        category: Category = Category.objects.create(
             name=category_data.get("name")
         )
 
-        response = {
+        response: dict = {
             "id": category.id,
             "name": category.name,
         }
@@ -117,15 +162,21 @@ class CategoryView(View):
 # Single category page (CBV)
 @method_decorator(csrf_exempt, name="dispatch")
 class CategoryDetailView(DetailView):
-    model = Category
+    model: Category = Category
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> JsonResponse:
+        """
+        Retrieve a single Category instance
+
+        :param request: The incoming request object
+        :return: JSON response with Category data
+        """
         try:
-            category = self.get_object()
+            category: Category = self.get_object()
         except Http404:
             return JsonResponse({"error": "Not found"}, status=404)
 
-        response = {
+        response: dict = {
             "id": category.id,
             "name": category.name,
         }
